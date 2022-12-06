ecf69ce656bea850a8b7e5aad614d0cf80ca45b8
moonhj <nmoonhj@gmail.com>
2022-12-06 23:57:32 +0900

    카카오 로그인 api 추가

---
+++ src/main/java/com/bside/server/global/login/KakaoUserInfo.java
@@ -0,0 +1,42 @@
+package com.bside.server.global.login;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
+public class KakaoUserInfo {
+
+  public static JsonNode getKakaoUserInfo(String accessToken) {
+    final String RequestUrl = "https://kapi.kakao.com/v2/user/me";
+    final HttpClient client = HttpClientBuilder.create().build();
+    final HttpPost post = new HttpPost(RequestUrl);
+
+    post.addHeader("Authorization", "Bearer " + accessToken);
+
+    JsonNode returnNode = null;
+
+    try {
+      final HttpResponse response = client.execute(post);
+      final int responseCode = response.getStatusLine().getStatusCode();
+
+      System.out.println("\nSending 'POST' request to URL : " + RequestUrl);
+      System.out.println("Response Code : " + responseCode);
+
+      ObjectMapper mapper = new ObjectMapper();
+      returnNode = mapper.readTree(response.getEntity().getContent());
+
+    } catch (ClientProtocolException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return returnNode;
+  }
+}
