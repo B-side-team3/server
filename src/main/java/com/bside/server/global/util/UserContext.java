aec83d99f0a08f07e2332febdd9807754576fa42
lee chaebeen <chaebeen@mailplug.co.kr>
2022-12-15 01:18:56 +0900

    UserContext 추가

---
+++ src/main/java/com/bside/server/global/util/UserContext.java
@@ -0,0 +1,25 @@
+package com.bside.server.global.util;
+
+import com.bside.server.global.auth.security.UserAdapter;
+import com.bside.server.member.domain.Member;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * SecurityContextHolder 사용하여
+ * 요청 별로 인증된 사용자 정보를 전역적으로 조회 가능하도록 한다.
+ */
+public final class UserContext {
+
+    private UserContext() {}
+
+    public static Member getMember() {
+        return getUserAdapter().getMember();
+    }
+
+    private static UserAdapter getUserAdapter() {
+        return (UserAdapter) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+    }
+}
