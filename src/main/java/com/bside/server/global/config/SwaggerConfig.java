08e831608244c1f230d05fc2721f3456d66f6275
moonhj <nmoonhj@gmail.com>
2022-12-24 23:05:17 +0900

    swagger 적용

---
+++ src/main/java/com/bside/server/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.bside.server.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+  @Bean
+  public Docket swaggerApi() {
+    return new Docket(DocumentationType.OAS_30)
+        .useDefaultResponseMessages(false)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.bside.server.module"))
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
