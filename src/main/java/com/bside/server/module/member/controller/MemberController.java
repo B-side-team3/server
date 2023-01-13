2736b83e9aedd0cd299a360bf180f7400bb84a46
moonhj <nmoonhj@gmail.com>
2023-01-14 00:06:02 +0900

    프로필 이미지 업로드 로직 구현

--- src/main/java/com/bside/server/module/member/controller/MemberController.java
+++ src/main/java/com/bside/server/module/member/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.bside.server.module.member.controller;
 
-import com.bside.server.module.member.dto.MemberRequest;
 import com.bside.server.module.member.dto.MemberResponse;
 import com.bside.server.module.member.service.MemberService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,16 +22,16 @@ public class MemberController {
     return memberService.getProfile();
   }
 
-  @ApiOperation(value = "프로필 수정")
-  @PatchMapping("/profile/update")
-  public MemberResponse updateProfile(@RequestBody MemberRequest request) {
-    return memberService.updateProfile(request);
+  @ApiOperation(value = "프로필 수정", httpMethod = "PATCH", produces = "multipart/form-data")
+  @PatchMapping(value = "/profile/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public MemberResponse updateProfile(String nickname, @ApiParam(value = "이미지 파일(/jpg, /png)") @RequestPart(required = false) MultipartFile imageFile) {
+    return memberService.updateProfile(nickname, imageFile);
   }
 
   @ApiOperation(value = "회원 탈퇴")
   @PatchMapping("/withdrawal")
-  public MemberResponse withdrawal() {
-    return memberService.withdrawal();
+  public void withdrawal() {
+    memberService.withdrawal();
   }
 
   @ApiOperation(value = "루틴 진행 조회")
--- src/main/java/com/bside/server/module/member/service/MemberService.java
+++ src/main/java/com/bside/server/module/member/service/MemberService.java
@@ -5,23 +5,32 @@ import com.bside.server.global.error.exception.AuthenticationException;
 import com.bside.server.global.util.UserContext;
 import com.bside.server.module.auth.service.AuthService;
 import com.bside.server.module.member.domain.Member;
-import com.bside.server.module.member.dto.MemberRequest;
 import com.bside.server.module.member.dto.MemberResponse;
 import com.bside.server.module.member.repository.MemberRepository;
 import com.bside.server.module.memberroutine.domain.MemberRoutine;
 import com.bside.server.module.memberroutine.repository.MemberRoutineRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+  @Value("${file-path.profileImage}")
+  private String filePath;
 
   private final MemberRepository memberRepository;
   private final MemberRoutineRepository memberRoutineRepository;
@@ -33,19 +42,45 @@ public class MemberService {
   }
 
   @Transactional
-  public MemberResponse updateProfile(MemberRequest request) {
+  public MemberResponse updateProfile(String nickname, MultipartFile imageFile) {
     Member member = memberRepository.findByMemberIdAndIsDeleted(UserContext.getMember().getMemberId(), false).orElseThrow(() -> new AuthenticationException(ErrorCode.UNKNOWN_USER));
-    member.setNickname(request.getNickname());
-//    member.setImageUrl(); /* todo 프로필 이미지 업로드 구현 예정
+    member.setNickname(nickname);
+
+    File file = new File(UserContext.getMember().getImageUrl() != null ? UserContext.getMember().getImageUrl() : "");
+
+    if (!ObjectUtils.isEmpty(imageFile)) {
+      if (file.exists()) {
+        file.delete();
+      }
+
+      UUID uuid = UUID.randomUUID();
+      String imageFileName = uuid + "_" + imageFile.getOriginalFilename();
+
+      Path imageFilePath = Paths.get(filePath + '/' + imageFileName);
+
+      try {
+        Files.write(imageFilePath, imageFile.getBytes());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      member.setImageUrl(imageFilePath.toString());
+    } else {
+      if (file.exists()) {
+        file.delete();
+      }
+      member.setImageUrl(null);
+    }
+
     return new MemberResponse(memberRepository.save(member));
   }
 
   @Transactional
-  public MemberResponse withdrawal() {
+  public void withdrawal() {
     Member member = memberRepository.findByMemberIdAndIsDeleted(UserContext.getMember().getMemberId(), false).orElseThrow(() -> new AuthenticationException(ErrorCode.UNKNOWN_USER));
     member.setDeleted(true);
     authService.revoke();
-    return new MemberResponse(memberRepository.save(member));
+    memberRepository.save(member);
   }
 
   public MemberResponse getRoutineProgress() {
