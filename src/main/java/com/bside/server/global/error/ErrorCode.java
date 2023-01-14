2b27b45d0702029fa999c34d59c3dc05262fdd4e
moonhj <nmoonhj@gmail.com>
2023-01-15 01:05:26 +0900

    file-path 변경

--- src/main/java/com/bside/server/global/error/ErrorCode.java
+++ src/main/java/com/bside/server/global/error/ErrorCode.java
@@ -57,8 +57,7 @@ public enum ErrorCode {
     NOTIFICATION_MEMBER_NOT_FOUND(7002, "사용자를 찾을 수 없습니다."),
 
     // 마이페이지
-    NOT_FOUND(8000, "정보를 찾을 수 없습니다.")
-    ;
+    NOT_FOUND(8000, "정보를 찾을 수 없습니다.");
 
     private final int code;
     private final String message;
